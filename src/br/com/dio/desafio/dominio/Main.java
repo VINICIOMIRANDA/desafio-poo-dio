2112b9ebafc74c8608a0c86e49166e59710567f4
Vinicio Miranda Covalski <vinicio.covalski@gmail.com>
2022-08-02 00:02:58 -0300

    Metodos Inscrever, progredir e calcularXPTOTAL

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -24,11 +24,49 @@ public class Main {
 		
 		
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria1);
+//		System.out.println(curso1);
+//		System.out.println(curso2);
+//		System.out.println(mentoria1);
 		
 	
+		Bootcamp bootcamp1 = new Bootcamp();
+		bootcamp1.setNome("Bootcamp Java Developer");
+		bootcamp1.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp1.getConteudos().add(curso1);
+		bootcamp1.getConteudos().add(curso2);
+		bootcamp1.getConteudos().add(mentoria1);
+		
+		Dev devVinicio = new Dev();
+		devVinicio.setNome("Vinicio");
+		devVinicio.inscreverBootcamp(bootcamp1);
+		
+		System.out.println("Conteudos Inscritos Vinicio" + devVinicio.getConteudosInscritos());
+		
+		devVinicio.progredir();
+		devVinicio.progredir();
+		devVinicio.progredir();
+		System.out.println("Conteudos ainda Inscritos Vinicio" + devVinicio.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Vinicio" + devVinicio.getConteudosConcluidos());
+		System.out.println("XP: "+ devVinicio.calcularTotalXp());
+		
+		System.out.println("----------------------------");
+		
+		Dev devFulano = new Dev();
+		devFulano.setNome("Fulano");
+		devFulano.inscreverBootcamp(bootcamp1);
+		System.out.println("Conteudos Inscritos Fulano" + devFulano.getConteudosInscritos());
+
+		devFulano.progredir();
+		System.out.println("Conteudos ainda Inscritos Fulano" + devFulano.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Fulano" + devFulano.getConteudosConcluidos());
+		System.out.println("XP: "+ devFulano.calcularTotalXp());
+
+
+
+		
+		
+
+
 		
 	}
 
